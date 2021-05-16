dafcb5d7b0866e5f03234e34be2743897ff05778
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-05-16 16:27:28 +0300

    add array to store the result

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ $(document).ready(function slotMachineImplementation() {
     const seven = 'src/7.png';
 
     const arr = [bar, barDoubled, barTripled, cherry, seven];
+    var resultArr = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+      ];
 
-    function spinEachTableCell(currentSlot, interval) {
+    function spinEachTableCell(currentSlot, interval, reel, reelIteration) {
         var counter = 0;
 
         var setIntervalId = setInterval(function runSlot(){
@@ -17,6 +22,7 @@ $(document).ready(function slotMachineImplementation() {
     
             if(counter === interval) {
                 clearInterval(setIntervalId);
+                resultArr[reel][reelIteration] = randImgIndex;
                 $(currentSlot).removeClass("blur");
                 $(currentSlot).removeClass("top");
                 return;
@@ -27,6 +33,7 @@ $(document).ready(function slotMachineImplementation() {
     $("#twist").on("click", function startSlotMachine() {
         $("img").addClass("blur");
         $("img").addClass("top");
+        document.getElementById('points').innerHTML = 'something';
 
         var table = document.getElementById('slotsCollection');
         var numberOfRows = table.rows.length;
@@ -36,8 +43,9 @@ $(document).ready(function slotMachineImplementation() {
 
             for(let j = 0; j < numberOfCells; j++) {
                 let currentCell = table.rows[i].cells[j].getElementsByTagName("*");
-                spinEachTableCell($(currentCell), 140 * (j + 1));
+                spinEachTableCell($(currentCell), 140 * (j + 1), i, j);
             }
         }
+        console.log('RESULT: ', resultArr);
     });
 });
