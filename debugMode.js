0a7df23b58a54bc9b6ce53a81d0bb790695e7c5b
Kseniia Prytkova <prytkovakseniia@gmail.com>
2021-05-18 12:46:56 +0300

    add debug zone

---
+++ debugMode.js
@@ -0,0 +1,7 @@
+function startDebugMode() {
+    var checkBox = document.getElementById("isDebug");
+    // var text = document.getElementById("text");
+    if (checkBox.checked == true){
+      console.log('debug mode');
+    }
+}
